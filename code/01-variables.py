08a02788f7f1f9b3700174c5cd3cdb6ebe7045c2
PramodCodes <147572924+PramodCodes@users.noreply.github.com>
2025-02-16 10:04:27 +0530

    Create 01-variables.py

---
+++ code/01-variables.py
@@ -0,0 +1,71 @@
+var = 123
+print(var)
+"""
+print(Var) 
+NameError: name 'Var' is not defined. Did you mean: 'var'? 
+block comment is """ and """
+inline comment is #
+"""
+print(type(var))
+
+""" types of data types with examples
+"""
+
+# Numeric datatypes:
+
+# int: e.g. 1, 2, 20, 40 etc
+# float: e.g. 1.22, 2.34, 4.35
+# complex: e.g. 1 + 2j, 4 + 5j
+
+# Boolean type: True, False
+
+# Sequence types:
+
+# str: 'a', "Hello world",
+
+a = 1
+print(a)
+
+b = 2.5
+print(b)
+
+c = 1 + 2j
+print(c)
+
+# know datatype of variable
+print(type(a))
+print(type(b))
+print(type(c))
+# boolean type
+a = True
+print(a)
+print(type(a))
+
+# String can be written in single quotes, double quotes or triple quotes
+
+a = 'a'
+print(a)
+print(type(a))
+
+a = "a"
+print(a)
+print(type(a))
+
+a = """a"""
+print(a)
+print(type(a))  
+
+str = 'Today\'s weather is sunny'
+print(str)
+
+# multi line string
+a = """
+This is a
+- multiline string
+"""
+print(a)
+
+
+
+
+
